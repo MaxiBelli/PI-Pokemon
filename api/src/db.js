056ecaf2f5bbf1d8eb291e53018f433f699986b5
Maxi Belli <maximiliano_belli@hotmail.com>
2021-08-02 21:06:56 -0300

    ...

--- api/src/db.js
+++ api/src/db.js
@@ -31,11 +31,17 @@ sequelize.models = Object.fromEntries(capsEntries);
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 const { Pokemon } = sequelize.models;
+const { Type } = sequelize.models;//
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+Pokemon.belongsToMany(Type, {through: 'PokemonType'} );//
+Type.belongsToMany(Pokemon, {through: 'PokemonType'} );//
+
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+  Pokemon,//
+  Type//
 };
--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,54 @@
+// const { DataTypes } = require('sequelize');
+// // Exportamos una funcion que define el modelo
+// // Luego le injectamos la conexion a sequelize.
+// module.exports = (sequelize) => {
+//   // defino el modelo
+//   sequelize.define('pokemon', {
+//     name: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//   });
+// };
+
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    img:{
+      type: DataTypes.TEXT
+    },
+    hp:{
+      type: DataTypes.INTEGER
+    },
+    attack:{
+      type: DataTypes.INTEGER,
+    },
+    defence:{
+      type: DataTypes.INTEGER
+    },
+    speed:{
+      type: DataTypes.INTEGER
+    },
+    height:{
+      type: DataTypes.INTEGER
+    },
+    weight:{
+      type: DataTypes.INTEGER
+    },
+    type:{
+      type: DataTypes.STRING
+    }
   });
 };
